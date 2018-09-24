c4f87c2d22306bb4bf4de015933f56824771fd9a
saper8111 <34764472+saper8111@users.noreply.github.com>
2018-09-24 15:33:34 +0300

    изменены настройки привязки к репозиторию

--- sandbox/src/main/java/ru/stqa/sanbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/sanbox/MyFirstProgram.java
@@ -1,9 +1,8 @@
 package ru.stqa.sanbox;
 
 public class MyFirstProgram {
-	
-	public static void main(String[] args) {
 
-System.out.println("Hello, world!");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
 }
